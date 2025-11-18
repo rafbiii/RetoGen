5746dbbe4e6d6883b8b906a3d6f0a13b070d6607
rafbiii <rafiqlabib0515@gmail.com>
2025-11-18 12:23:05 +0700

    delete all backend

--- Backend/models/user.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel, EmailStr
-
-class User(BaseModel):
-    username: str
-    email: EmailStr
-    password: str
-    name: str
--- Backend/schemas/login_schema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-class LoginUser(BaseModel):
-    email: str
-    password: str
